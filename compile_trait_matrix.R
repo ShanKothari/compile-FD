91d93dcda30483db0c0a0d369c4509a08b9b5e75
Shan Kothari <quercusacerifolia@gmail.com>
2022-07-15 20:07:57 -0400

    read in data

--- compile_trait_matrix.R
+++ compile_trait_matrix.R
@@ -16,9 +16,71 @@ trait_sp_split<-strsplit(trait_sub$scientific_name,split=" ")
 trait_sub$genus<-unlist(lapply(trait_sp_split,function(el) el[[1]]))
 trait_sub$species<-unlist(lapply(trait_sp_split,function(el) el[[2]]))
 trait_sub$species[which(trait_sub$species!=tolower(trait_sub$species))]<-NA
+trait_sub$binomial<-paste(trait_sub$genus,trait_sub$species,sep=" ")
+
+trait_sub$N_CABO<-trait_sub$trait_n_perc
+trait_sub$N_TRYsp<-trait_sub$trait_n_perc
+trait_sub$N_TRYgn<-trait_sub$trait_n_perc
+trait_sub$LMA_CABO<-trait_sub$trait_LMA_perc
+trait_sub$LMA_TRYsp<-trait_sub$trait_LMA_perc
+trait_sub$LMA_TRYgn<-trait_sub$trait_LMA_perc
+trait_sub$LDMC_CABO<-trait_sub$trait_LDMC_perc
+trait_sub$LDMC_TRYsp<-trait_sub$trait_LDMC_perc
+trait_sub$LDMC_TRYgn<-trait_sub$trait_LDMC_perc
 
 ###################################
-##
+## read CABO data
 
 ref_traits<-readRDS("../FreshLeafModels/ProcessedSpectra/all_ref_and_traits.rds")
 ref_meta<-meta(ref_traits)
+
+ref_meta_agg<-aggregate(ref_meta[,c("LMA","LDMC","Nmass")],
+                        by=list(ref_meta$species),
+                        FUN=mean,na.rm=T)
+
+###################################
+## read TRY data
+
+TRY_N<-read.csv("../FreshLeafModels/TraitData/TRY/TRY_N.csv")
+TRY_N<-TRY_N[-which(TRY_N$SpeciesName==""),]
+TRY_N_sp<-strsplit(as.character(TRY_N$SpeciesName),split=" ")
+TRY_N$genus<-unlist(lapply(TRY_N_sp,function(el) el[[1]]))
+TRY_N$species<-unlist(lapply(TRY_N_sp,function(el){
+  if(length(el)>1){
+    return(el[[2]])
+  } else{
+    return(NA)
+  }}))
+
+TRY_SLA<-read.csv("../FreshLeafModels/TraitData/TRY/TRY_SLA.csv")
+TRY_SLA_sp<-strsplit(as.character(TRY_SLA$SpeciesName),split=" ")
+TRY_SLA$genus<-unlist(lapply(TRY_SLA_sp,function(el) el[[1]]))
+TRY_SLA$species<-unlist(lapply(TRY_SLA_sp,function(el){
+  if(length(el)>1){
+    return(el[[2]])
+  } else{
+    return(NA)
+  }}))
+
+TRY_LDMC<-read.csv("../FreshLeafModels/TraitData/TRY/TRY_LDMC.csv")
+TRY_LDMC_sp<-strsplit(as.character(TRY_LDMC$SpeciesName),split=" ")
+TRY_LDMC$genus<-unlist(lapply(TRY_LDMC_sp,function(el) el[[1]]))
+TRY_LDMC$species<-unlist(lapply(TRY_LDMC_sp,function(el){
+  if(length(el)>1){
+    return(el[[2]])
+  } else{
+    return(NA)
+  }}))
+
+#########################################
+## attach data to matrix
+
+for(i in 1:nrow(trait_sub)){
+  
+  ## fill in N
+  if(is.na(trait_sub$trait_n_perc)){
+    
+  }
+    
+  ## fill in LMC
+}
