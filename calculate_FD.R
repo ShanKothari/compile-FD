3409546e4a47b9b42d4c577d12db53e17a0d2084
Shan Kothari <quercusacerifolia@gmail.com>
2022-07-15 22:29:45 -0400

    Update calculate_FD.R

--- calculate_FD.R
+++ calculate_FD.R
@@ -1,6 +1,11 @@
 setwd("C:/Users/kotha020/Dropbox/PostdocProjects/WallisFunctionalTraits/")
 
 trait_matrix<-read.csv("filled_trait_matrix.csv")
+all_traits_site<-which(!is.na(trait_matrix$) & !is.na(trait_matrix$LMA_site) & !is.na(trait_matrix$LDMC_site))
+all_traits_CABO<-which(!is.na(trait_matrix$N_CABO) & !is.na(trait_matrix$LMA_CABO) & !is.na(trait_matrix$LDMC_CABO))
+all_traits_sp<-which(!is.na(trait_matrix$N_TRYsp) & !is.na(trait_matrix$LMA_TRYsp) & !is.na(trait_matrix$LDMC_TRYsp))
+all_traits_gn<-which(!is.na(trait_matrix$N_TRYgn) & !is.na(trait_matrix$LMA_TRYgn) & !is.na(trait_matrix$LDMC_TRYgn))
+
 IGB_trait_matrix<-trait_matrix[trait_matrix$project=="IGB",]
 MB_trait_matrix<-trait_matrix[trait_matrix$project=="MB",]
 CGOP_trait_matrix<-trait_matrix[trait_matrix$project=="CGOP",]
